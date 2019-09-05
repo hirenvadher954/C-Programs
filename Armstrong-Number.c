c37e4f6c0311ce24bfbe738bde18212515ff426c
Hiren Vadher <hirenvadher954@gmail.com>
2019-09-05 16:19:52 +0530

    Create Armstrong-Number.c

---
+++ Armstrong-Number.c
@@ -0,0 +1,29 @@
+// To check wheather number is Armstrong number or Not
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int number,extractDigit,sum = 0,numberofPower = 0;
+    printf("Enter Any Number:- ");
+    scanf("%d",&number);
+    
+    int orignalNumber = number;
+    
+    while(number!=0){
+        number = number/10;
+        numberofPower++;
+    }
+    number = orignalNumber;
+    while(number!=0){
+        extractDigit = number%10;
+        sum = sum + pow(extractDigit,numberofPower);
+        number = number/10;
+       }
+    
+    if(orignalNumber == sum)
+        printf("Armstrong number");
+    else
+        printf("Not Armstrong number");
+    
+}
